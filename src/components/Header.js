68f150837096f2e0e7be78475803830265ffeb53
niroavram <69424993+niroavram@users.noreply.github.com>
2021-11-09 17:12:28 +0200

    informtion

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,18 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import useStyles from "./map/style";
+import DialogInformaton from "./Inforamtion";
 const logo = "https://res.cloudinary.com/niroavram/image/upload/v1636356766/logo_ukicpq.png"
+
 
 export default function Header() {
   const classes = useStyles();
 
   return (
-    <div style={{backgroundColor:"#f8f9fa",borderWidth:0.5,borderRadius:15,borderColor:"#9bf6ff",justifyContent:'center',textAlign:'center'}} >
-    <img src={logo} alt="Logo" width="150vw"  height="50vh"/>
+    <div style={{backgroundColor:"#f8f9fa",borderWidth:0.5,borderRadius:15,borderColor:"#9bf6ff",justifyContent:'center',textAlign:'center',display: 'flex'}} >
+    <img src={logo} alt="Logo" width="150vw"  height="35vh"/>
+    <DialogInformaton />
+
     </div>
   );
 }
